aeca668108d6b5cf5f2b1920b9c8f418e7b8f39e
tasnim123 <tasnimnafi123@gmail.com>
2022-03-07 01:47:49 -0500

    Added questions

--- index.js
+++ index.js
@@ -3,6 +3,52 @@ const fs = require('fs');
 const fileName = process.argv[2];
 
 const questions = [
+  {
+    name: 'name',
+    message: 'What is the project name?',
+    type: 'input'
+},
+{
+    name: 'about',
+    message: 'What kind of project it is(Front or Back)? ',
+    type: 'input'
+},
+{
+    name: 'requirments',
+    message: 'What are the requirments to run this project? ',
+    type: 'input'
+},
+{
+    name: 'Run information',
+    message: 'How can you run this project?',
+    type: 'input'
+},
+{
+    name: 'contribution-guidelines',
+    message: 'Enter the contribution guidelines: ',
+    type: 'input'
+},
+{
+    name: 'test',
+    message: 'Enter the test instructions: ',
+    type: 'input'
+},
+{
+    message: "Select the lisence",
+    name: 'lisence',
+    choices: ['Apache 2.0 License', 'BSD 3-Clause License', 'BSD 2-Clause License'],
+    type: 'list'
+},
+{
+    name: 'github-username',
+    message: 'Enter the GitHub username: ',
+    type: 'input'
+},
+{
+    name: 'email',
+    message: 'Enter the email address: ',
+    type: 'input'
+},
   
 ];
 
