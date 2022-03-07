4275c4d218b4d5379242ffbde5a318636753970f
tasnim123 <tasnimnafi123@gmail.com>
2022-03-07 01:44:05 -0500

    Setup Basic File Structure

---
+++ index.js
@@ -0,0 +1,16 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const fileName = process.argv[2];
+
+const questions = [
+  
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+
+function init() {}
+
+// Function call to initialize app
+init();
